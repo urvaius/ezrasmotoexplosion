97afbc56b598774b73887186fbfa28e2742a081f
Dave Arneson <urvaius@gmail.com>
2014-03-27 12:24:01 -0700

    starting to work on

--- core/src/com/arne5/ezrasmotoexplosion/EzrasMotoExplosion.java
+++ core/src/com/arne5/ezrasmotoexplosion/EzrasMotoExplosion.java
@@ -13,14 +13,16 @@ public class EzrasMotoExplosion extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
+        
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
