d2ad7806a779927c9517905c97361a106d1cad59
Dave Arneson <urvaius@gmail.com>
2014-04-04 11:16:29 -0700

    desktop and android working

--- android/src/com/arne5/ezrasmotoexplosion/android/AndroidLauncher.java
+++ android/src/com/arne5/ezrasmotoexplosion/android/AndroidLauncher.java
@@ -2,6 +2,7 @@ package com.arne5.ezrasmotoexplosion.android;
 
 import android.os.Bundle;
 
+import com.arne5.ezrasmotoexplosion.EzrasMotoExplosion;
 import com.arne5.ezrasmotoexplosion.GameScreen;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -13,6 +14,8 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useAccelerometer = false;
         config.useCompass = false;
-		initialize(new GameScreen(), config);
+		//initialize(new GameScreen(), config);
+        initialize(new EzrasMotoExplosion(),config);
+
 	}
 }
